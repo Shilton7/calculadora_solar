f7e7cda8ee0318847c50e564478b64837375732a
Shilton <shilton.sistemas@gmail.com>
2019-09-20 17:28:50 -0300

    Configurando bootstrap, instalando axios e bootstrap

---
+++ calculadora_solar/src/components/calculadora.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'calculdadora',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ calculadora_solar/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './core/config/bootstrap'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
