c8aca95467ec00a005e7f3bce2b43d4c94ee50f6
Shilton <shilton.sistemas@gmail.com>
2019-10-01 18:33:06 -0300

    Criando os Cálculos

--- calculadora/src/components/Calculadora.vue
+++ calculadora/src/components/Calculadora.vue
@@ -5,7 +5,7 @@
 
 <br>
 
-<div class="col-md-8 jumbotron" v-if="formCidade == 'S'">
+<div class="col-md-12 jumbotron" v-if="formCidade == 'S'">
   <h3>Qual a sua localização?</h3>
   <form>
   <div class="form-group">
@@ -34,7 +34,7 @@
 </form>
 </div>
 
-<div class="col-md-8 jumbotron" v-if="formValConta == 'S'">
+<div class="col-md-12 jumbotron" v-if="formValConta == 'S'">
   <form>
   <div class="form-group">
     <label for="exampleInputEmail1">Qual o valor mensal da sua conta de luz?</label>
@@ -45,7 +45,7 @@
 </form>
 </div>
 
-<div class="col-md-8 jumbotron" v-if="formConsumo == 'S'">
+<div class="col-md-12 jumbotron" v-if="formConsumo == 'S'">
   <form>
   <div class="form-group">
     <label for="exampleInputEmail1">Qual o seu consumo médio de kwH? Veja na sua conta de luz.</label>
@@ -56,7 +56,7 @@
 </form>
 </div>
 
-<div class="col-md-8 jumbotron" v-if="formLocalInstal == 'S'">
+<div class="col-md-12 jumbotron" v-if="formLocalInstal == 'S'">
   <form>
   <div class="form-group">
     <label for="exampleInputPassword1">Onde você gostaria de colocar seu gerador solar? </label>
@@ -74,16 +74,64 @@
 </div>
 
 
-<div class="col-md-4">
+<div class="col-md-12" v-if="tbl_resultado == 'S'">
     <h3>Resultados</h3>
     <br>
 
-    <span class="label label-primary"><strong>Estado: </strong> {{this.estado}}</span> <br>
-    <span class="label label-primary"><strong>Cidade: </strong> {{this.cidade}}</span> <br>
-    <span class="label label-primary"><strong>Valor da conta de luz:</strong> {{this.valContaLuz}}</span> <br>
-    <span class="label label-primary"><strong>Consumo: </strong> {{this.valConsumo}}</span> <br>
-    <span class="label label-primary"><strong>Local instalação: </strong> {{this.localInstal}}</span>
+    <table class="table">
+    <caption>Optional table caption.</caption>
+    <thead>
+        <tr>
+            <th>Potência mínima painéis</th>
+            <th>Retorno do investimento</th>
+            <th>Economia</th>
+            <th>Área mínima ocupada:</th>
+            <th>Peso da estrutura</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td> {{calculoPotenciaMinima()}} </td>
+            <td> {{calculoRetornoInvest()}} </td>
+            <td> R$ {{calculoEconomiaAno()}} por ano e  R$ {{calculoEconomiaEmAnos()}} em 20 anos </td>
+            <td> {{calculoAreaOcupada()}} m² </td>
+            <td> {{calculorPesoEstru()}} kg </td>
+        </tr>
 
+    </tbody>
+</table>
+
+    <h2>
+        <span class="label label-primary"><strong>Estado: </strong> {{this.estado}}</span> <br><br>
+        <span class="label label-primary"><strong>Cidade: </strong> {{this.cidade}}</span> <br><br>
+        <span class="label label-primary"><strong>Valor da conta de luz:</strong> {{this.valContaLuz}}</span> <br><br>
+        <span class="label label-primary"><strong>Consumo: </strong> {{this.valConsumo}}</span> <br><br>
+        <span class="label label-primary"><strong>Local instalação: </strong> {{this.localInstal}}</span><br><br>
+        <span class="label label-primary"><strong>Valor incidência: </strong> {{this.valIndicendiaSolar}}</span> <br><br>
+        <span class="label label-primary"><strong>Valor Eficiência da Placa: </strong> {{this.valEficienciaPlaca}}</span> <br><br>
+
+        <span class="label label-primary"><strong>Potência mínima painés: </strong>
+        {{calculoPotenciaMinima()}} </span> <br><br>
+
+        <span class="label label-primary"><strong>Quantidade de placas: </strong>
+        {{calculoQuantPlacas()}} </span> <br><br>
+
+        <span class="label label-primary"><strong>Retorno do seu investimento: </strong>
+        {{calculoRetornoInvest()}}
+        </span> <br><br>
+
+        <span class="label label-primary"><strong>Economizaria: </strong>
+          R$ {{calculoEconomiaAno()}} por ano e  R$ {{calculoEconomiaEmAnos()}} em 20 anos
+        </span> <br><br>
+
+        <span class="label label-primary"><strong>Área mínima ocupada: </strong>
+        {{calculoAreaOcupada()}} m²
+        </span> <br><br>
+
+        <span class="label label-primary"><strong>Peso da estrutura: </strong>
+        {{calculorPesoEstru()}} kg
+        </span> <br>
+    </h2>
 
 </div>
 
@@ -91,6 +139,7 @@
 </template>
 
 <script>
+
 export default {
   name: 'Calculadora',
   data () {
@@ -99,11 +148,30 @@ export default {
       formConsumo: 'N',
       formValConta: 'N',
       formLocalInstal: 'N',
+      tbl_resultado: 'S',
       estado: 'Rio de Janeiro',
       cidade: null,
       valContaLuz: null,
       valConsumo: null,
-      localInstal: null
+      localInstal: null,
+      potencia_min: null,
+      quant_placas: null,
+      retorno_fin:null,
+      economia:null,
+      area_ocupada:null,
+      peso_estrutura: null,
+      valIndicendiaSolar: '4.2',
+      valEficienciaPlaca: '0.83',
+      valPotenciaPlaca: '365',
+      valCustoPlaca: '1.700',
+      anual: '12',
+      economizaAno: null,
+      economizaEmAnos: null,
+      area_placa: '2.5',
+      valAreaOcupada: null,
+      pesoPlaca: '32.5',
+      valPesoEstrutura: null
+
     }
   },
   methods: {
@@ -124,7 +192,41 @@ export default {
       this.formConsumo = 'N',
       this.formValConta = 'N',
       this.formLocalInstal = 'N'
-
+      this.tbl_resultado = 'S'
+    },
+    calculoPotenciaMinima () {
+      let $valor = ((this.valConsumo / 30) / (this.valIndicendiaSolar * this.valEficienciaPlaca))
+      this.potencia_min = parseFloat($valor.toFixed(2))
+      return parseFloat($valor.toFixed(2))
+    },
+    calculoQuantPlacas () {
+          let $valor = (this.potencia_min / (this.valPotenciaPlaca / 1000))
+          this.quant_placas = parseFloat(Math.round($valor.toFixed(2)))
+          return parseFloat(Math.round($valor.toFixed(2)))
+    },
+    calculoRetornoInvest () {
+      let $valor = ((this.valCustoPlaca * this.quant_placas) / this.anual )
+      return parseFloat(Math.round($valor.toFixed(2)))
+    },
+    calculoEconomiaAno () {
+      let $valor = this.valContaLuz * this.anual
+      this.economizaAno = parseFloat(Math.round($valor.toFixed(2)))
+      return parseFloat(Math.round($valor.toFixed(2)))
+    },
+    calculoEconomiaEmAnos () {
+      let $valor = this.economizaAno * 20
+      this.economizaEmAnos = parseFloat(Math.round($valor.toFixed(2)))
+      return parseFloat(Math.round($valor.toFixed(2)))
+    },
+    calculoAreaOcupada () {
+      let $valor = this.quant_placas * this.area_placa
+      this.valAreaOcupada = parseFloat(Math.round($valor.toFixed(2)))
+      return parseFloat(Math.round($valor.toFixed(2)))
+    },
+    calculorPesoEstru () {
+      let $valor = this.quant_placas * this.pesoPlaca
+      this.valPesoEstrutura = parseFloat(Math.round($valor.toFixed(2)))
+      return parseFloat(Math.round($valor.toFixed(2)))
     }
   }
 }
--- calculadora_solar/src/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './core/config/bootstrap'
-
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
